d53335605c67cf052e7cea08fac4212a27ed727e
Mert <156681170+Mert-409@users.noreply.github.com>
2024-09-08 13:43:44 +0300

    Update idx-expo-templates.js

--- idx-expo-templates.js
+++ idx-expo-templates.js
@@ -1,5 +1,3 @@
-// Bash script is recommended.
-
 // Before using run npm install fs-extra
 
 #!/usr/bin/env node
@@ -8,43 +6,56 @@ const { execSync } = require('child_process');
 const fs = require('fs-extra');
 const path = require('path');
 
-// Proje ismini al
 const projectName = process.argv[2] || 'rn-proje';
 
-function moveIdxFolder() {
-  const homeDir = process.env.HOME || process.env.USERPROFILE;
-  const source = path.join(homeDir, '.idx');
-  const destination = path.resolve(projectName, '.idx');
+const homeDir = process.env.HOME || process.env.USERPROFILE;
+const idxPath = path.join(homeDir, '.idx');
 
-  if (fs.existsSync(source)) {
-    console.log('Moving .idx folder...');
-    fs.copySync(source, destination);
+function moveIdxToHome(projectDir) {
+  const projectIdxPath = path.join(projectDir, '.idx');
+  
+  if (fs.existsSync(projectIdxPath)) {
+    console.log('.idx klasörü home/user dizinine taşınıyor...');
+    fs.moveSync(projectIdxPath, idxPath, { overwrite: true });
   } else {
-    console.log('Can't found .idx folder.');
+    console.log('.idx klasörü proje dizininde bulunamadı.');
   }
 }
 
 function deleteCurrentProject() {
-  console.log('Deleting old project...');
+  console.log('Mevcut projeyi siliyorum...');
   fs.removeSync(path.resolve(projectName));
 }
 
 function createExpoApp() {
-  console.log('Creating new Expo project...');
+  console.log('Yeni Expo projesi oluşturuluyor...');
   execSync(`npx create-expo-app ${projectName} --template blank --yes`, { stdio: 'inherit' });
 }
 
+function moveIdxToNewProject() {
+  const newProjectIdxPath = path.resolve(projectName, '.idx');
+
+  if (fs.existsSync(idxPath)) {
+    console.log('.idx klasörü yeni projeye taşınıyor...');
+    fs.moveSync(idxPath, newProjectIdxPath, { overwrite: true });
+  } else {
+    console.log('home/user dizininde .idx klasörü bulunamadı.');
+  }
+}
+
 function runAndroid() {
-  console.log('Starting Android emulator...');
-  execSync(`cd ${projectName} && npm run android`, { stdio: 'inherit' });
+  console.log('Android emülatörü başlatılıyor...');
+  execSync(`cd ${projectName} && npx expo run:android`, { stdio: 'inherit' });
 }
 
 function main() {
-  moveIdxFolder(); 
-  deleteCurrentProject();
-  createExpoApp();
-
-  runAndroid();
+  const projectDir = path.resolve(projectName);
+  
+  moveIdxToHome(projectDir);   // 1. Adım: .idx'i home/user'a taşı
+  deleteCurrentProject();      // 2. Adım: Eski projeyi sil
+  createExpoApp();             // 3. Adım: Yeni proje oluştur
+  moveIdxToNewProject();       // 4. Adım: .idx'i yeni projeye taşı
+  runAndroid();                // 5. Adım: Android emülatörünü başlat
 }
 
 main();
